a947c4f986eeb0fac7188b5f140ee5e7df996af8
zsx <zsx@zsxsoft.com>
2015-03-15 13:01:58 +0800

    移动文件路径

--- app.js
+++ app.js
@@ -4,14 +4,14 @@
 	var async = require("async");
 	var config = global.config = require('./config');
 	// 公用函数
-	global.utils = require("./utils");
+	global.utils = require("./lib/utils");
 
 	// 事件处理
 	global.coordinator = new (require('events').EventEmitter);
 
 	// 加载模块
-	async.each(["./child", "./http", "./socket", "./check"], function(module, callback) {
-		require(module).init(callback);
+	async.each(["child", "http", "socket", "check"], function(module, callback) {
+		require("./lib/" + module).init(callback);
 	}, function(err) {
 		log.log("Init completed!");
 	});
