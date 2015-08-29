91449d35c1cb330ce68f9c5bda746f6942e8ccfb
zsx <zsx@zsxsoft.com>
2015-08-29 17:51:59 +0800

    检测DaoCloud的MySQL服务

--- app.js
+++ app.js
@@ -10,13 +10,22 @@ global.config = require('./config');
 // 公用函数
 require("./lib/utils");
 
+// 检测DaoCloud的MySQL服务
+if (process.env.MYSQL_PORT_3306_TCP_PORT) {
+	var dbPos = config.database;
+	dbPos.type = "mysql";
+	dbPos.server = process.env.MYSQL_PORT_3306_TCP_ADDR;
+	dbPos.username = process.env.MYSQL_USERNAME;
+	dbPos.password = process.env.MYSQL_PASSWORD;
+	dbPos.port = process.env.MYSQL_PORT_3306_TCP_ADDR;
+	dbPos.db = process.env.MYSQL_INSTANCE_NAME;
+}
 
 // 加载模块
 async.map(["ext", "cache", "transfer", "database", "http", "socket"], function (module, callback) {
 	require("./lib/" + module).init(callback);
 }, function (err) {
-		coordinator.emit("configUpdated");
-		log.log("服务器初始化完成");
-	}
-	);
+	coordinator.emit("configUpdated");
+	log.log("服务器初始化完成");
+});
 
